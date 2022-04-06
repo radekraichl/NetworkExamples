57e6e51901d83cb9606c1416fbc511ad76e12b02
Radek Raichl <radekraichl@gmail.com>
2022-04-06 23:35:58 +0200

    Update project OpenWeatherMap

--- OpenWeatherMap/OpenWeatherMapDataProvider.cs
+++ OpenWeatherMap/OpenWeatherMapDataProvider.cs
@@ -3,54 +3,63 @@ using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 
-namespace OpenWeatherMap
+namespace OpenWeatherMap;
+
+internal class OpenWeatherMapDataProvider
 {
-    class OpenWeatherMapDataProvider
+    private readonly string API_KEY = "3af09621cb03ff4438fd934032c2f0e9";
+
+    public async Task<WeatherInfo> GetWeatherByCityNameAsync(string cityName)
     {
-        private readonly string API_KEY = "3af09621cb03ff4438fd934032c2f0e9";
+        string request = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&units=metric&appid={API_KEY}&lang=cz";
 
-        public async Task<WeatherInfo> GetWeatherByCityName(string cityName)
+        return await GetWeatherInfo(request);
+    }
+
+    public async Task<WeatherInfo> GetWeatherByCityIDAsync(string id)
+    {
+        string request = $"https://api.openweathermap.org/data/2.5/weather?id={id}&units=metric&appid={API_KEY}&lang=cz";
+
+        return await GetWeatherInfo(request);
+    }
+
+    public static async Task<string> GetJsonFromRequestAsync(string request)
+    {
+        using HttpClient httpClient = new();
+        using HttpResponseMessage response = await httpClient.GetAsync(request);
+
+        if (response.IsSuccessStatusCode)
         {
-            string request =
-                $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&units=metric&appid={API_KEY}&lang=cz";
+            string jsonString = await response.Content.ReadAsStringAsync();
 
-            return await GetWeatherInfo(request);
+            JsonDocument jsonDocument = JsonDocument.Parse(jsonString);
+
+            return JsonSerializer.Serialize(jsonDocument, new JsonSerializerOptions { WriteIndented = true });
         }
 
-        public async Task<WeatherInfo> GetWeatherByCityID(string id)
+        return null;
+    }
+
+    private static async Task<WeatherInfo> GetWeatherInfo(string request)
+    {
+        string jsonString = await GetJsonFromRequestAsync(request);
+
+        if (jsonString != null)
         {
-            string request =
-                $"https://api.openweathermap.org/data/2.5/weather?id={id}&units=metric&appid={API_KEY}&lang=cz";
+            using JsonDocument jsonDocument = JsonDocument.Parse(jsonString);
 
-            return await GetWeatherInfo(request);
-        }
-
-        private static async Task<WeatherInfo> GetWeatherInfo(string request)
-        {
-            using HttpClient httpClient = new();
-            HttpResponseMessage response = await httpClient.GetAsync(request);
-
-            if (response.IsSuccessStatusCode)
+            return new WeatherInfo
             {
-                string jsonString = await response.Content.ReadAsStringAsync();
-
-                //Console.WriteLine(jsonString);
-
-                JsonDocument jsonDocument = JsonDocument.Parse(jsonString);
-
-                return new WeatherInfo
-                {
-                    Longitude = (float)jsonDocument.RootElement.GetProperty("coord").GetProperty("lon").GetDouble(),
-                    Latitude = (float)jsonDocument.RootElement.GetProperty("coord").GetProperty("lat").GetDouble(),
-                    WeatherMain = jsonDocument.RootElement.GetProperty("weather")[0].GetProperty("main").GetString(),
-                    WeatherDescription = jsonDocument.RootElement.GetProperty("weather")[0].GetProperty("description").GetString(),
-                    Temperature = (float)jsonDocument.RootElement.GetProperty("main").GetProperty("temp").GetDouble(),
-                    Humidity = (int)jsonDocument.RootElement.GetProperty("main").GetProperty("humidity").GetDouble(),
-                    Pressure = (int)jsonDocument.RootElement.GetProperty("main").GetProperty("pressure").GetDouble(),
-                };
-            }
-
-            return null;
+                Longitude = (float)jsonDocument.RootElement.GetProperty("coord").GetProperty("lon").GetDouble(),
+                Latitude = (float)jsonDocument.RootElement.GetProperty("coord").GetProperty("lat").GetDouble(),
+                WeatherMain = jsonDocument.RootElement.GetProperty("weather")[0].GetProperty("main").GetString(),
+                WeatherDescription = jsonDocument.RootElement.GetProperty("weather")[0].GetProperty("description").GetString(),
+                Temperature = (float)jsonDocument.RootElement.GetProperty("main").GetProperty("temp").GetDouble(),
+                Humidity = (int)jsonDocument.RootElement.GetProperty("main").GetProperty("humidity").GetDouble(),
+                Pressure = (int)jsonDocument.RootElement.GetProperty("main").GetProperty("pressure").GetDouble(),
+            };
         }
+
+        return null;
     }
 }
