8e3cdc6223f91b086d0eb87e05824bebcc7ef6b9
Radek Raichl <radekraichl@gmail.com>
2022-04-23 11:13:05 +0200

    Add config to OpenWeatherMap

--- OpenWeatherMap/OpenWeatherMapDataProvider.cs
+++ OpenWeatherMap/OpenWeatherMapDataProvider.cs
@@ -6,17 +6,17 @@ namespace OpenWeatherMap;
 
 internal class OpenWeatherMapDataProvider
 {
-    private readonly string API_KEY = "3af09621cb03ff4438fd934032c2f0e9";
+    private readonly string apiKey = Config.API_KEY;
 
     public async Task<WeatherInfo> GetWeatherByCityNameAsync(string cityName)
     {
-        string request = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&units=metric&appid={API_KEY}&lang=cz";
+        string request = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&units=metric&appid={apiKey}&lang=cz";
         return await GetWeatherInfoAsync(request);
     }
 
     public async Task<WeatherInfo> GetWeatherByCityIDAsync(string id)
     {
-        string request = $"https://api.openweathermap.org/data/2.5/weather?id={id}&units=metric&appid={API_KEY}&lang=cz";
+        string request = $"https://api.openweathermap.org/data/2.5/weather?id={id}&units=metric&appid={apiKey}&lang=cz";
         return await GetWeatherInfoAsync(request);
     }
 
